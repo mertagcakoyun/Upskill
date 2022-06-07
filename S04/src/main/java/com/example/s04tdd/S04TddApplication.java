fb6976b3b73e7567b453164483f61fca1cea65db
Mert Agcakoyun <mert.agcakoyun@trendyol.com>
2022-06-07 09:23:38 +0300

    git init

---
+++ S04/src/main/java/com/example/s04tdd/S04TddApplication.java
@@ -0,0 +1,13 @@
+package com.example.s04tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04TddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S04TddApplication.class, args);
+    }
+
+}
