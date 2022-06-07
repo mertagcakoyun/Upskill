a13f749a21138b73a379ff295a975c4b0103fc12
omer.selvi <omer.selvi@trendyol.com>
2022-06-07 11:40:35 +0300

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:S04/src/test/java/com/example/s04tdd/S04TddApplicationTests.java

---
+++ S04/src/test/java/com/example/s04tdd/Fibonacci.java
@@ -0,0 +1,7 @@
+package com.example.s04tdd;
+
+public class Fibonacci {
+    public Integer findNthElement(int elem) {
+        return 0;
+    }
+}
--- S04/src/test/java/com/example/s04tdd/S04TddApplicationTests.java
+++ S04/src/test/java/com/example/s04tdd/S04TddApplicationTests.java
@@ -3,11 +3,24 @@ package com.example.s04tdd;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class S04TddApplicationTests {
 
+    // when n=1
+    // when n=2
     @Test
-    void contextLoads() {
+    void shouldReturnZeroWhenNEqualsOne() {
+
+        assertThat(new Fibonacci().findNthElement(1)).isEqualTo(0);
+    }
+
+    @Test
+    void shouldReturnOneWhenNEqualsTwo() {
+
+        assertThat(new Fibonacci().findNthElement(1)).isEqualTo(0);
     }
 
 }
