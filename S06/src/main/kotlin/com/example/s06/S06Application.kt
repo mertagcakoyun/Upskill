301517eee63435b66ca0d8e8b814fb67cedb940b
Mert Agcakoyun <mert.agcakoyun@trendyol.com>
2022-06-14 09:25:22 +0300

    Transformation Priority Premise exercises

---
+++ S06/src/main/kotlin/com/example/s06/S06Application.kt
@@ -0,0 +1,11 @@
+package com.example.s06
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class S06Application
+
+fun main(args: Array<String>) {
+    runApplication<S06Application>(*args)
+}
---
+++ S06/src/test/kotlin/com/example/s06/S06ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.s06
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class S06ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
